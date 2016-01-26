9fe1cad9ee914f6a86e60e74447eba88338afd0f
David Fonollosa <david.fonollosa@innofis.com>
2016-01-26 15:40:23 +0100

    setup application config

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,20 +1,25 @@
 (function(){
-	angular.module('PatientsApp', [
-	                                    'ngRoute'
-	                                    ]).config(function ( $routeProvider ) {
-	                                    	'use strict';
-	                                    	$routeProvider
-	                                    	.when('/patients', {
-	                                    		templateUrl: 'views/patients.html',
-	                                    		controller: 'PatientsCtrl',
-	                                    		controllerAs: 'patients'
-	                                    	}) 
-	                                    	.otherwise({
-	                                    		redirectTo: '/patients'
-	                                    	});
-	                                    }).run(function($rootScope){
-	                                    	$rootScope.$on('$routeChangeError', function(event, current, previous, rejection){
-	                                    		console.log(event, current, previous, rejection)
-	                                    	});
-	                                    });
+	function appConfig ( $routeProvider ) {
+    	'use strict';
+    	$routeProvider
+	    	.when('/patients', {
+	    		templateUrl: 'views/patients.html',
+	    		controller: 'PatientsCtrl',
+	    		controllerAs: 'patients'
+	    	}) 
+	    	.otherwise({
+	    		redirectTo: '/patients'
+	    	});
+    }
+	
+	function appRun ($rootScope){
+    	$rootScope.$on('$routeChangeError', function(event, current, previous, rejection){
+    		console.log(event, current, previous, rejection)
+    	});
+    }
+	
+	angular
+		.module('PatientsApp', ['ngRoute'])
+		.config(appConfig)
+		.run(appRun);
 })()
